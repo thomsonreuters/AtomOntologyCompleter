feb6c979fc62689f86399c307e0fead552988b9e
Dan Bennett <dan.bennett@thomsonreuters.com>
2018-05-25 08:46:19 -0500

    Added all the current ontologies

--- lib/ontologies/generate.sh
+++ lib/ontologies/generate.sh
@@ -7,9 +7,16 @@ cat dc11.ttl | python convert.py "http://dublincore.org/documents/dcmi-terms/#te
 cat dcat.ttl | python convert.py "https://www.w3.org/TR/vocab-dcat/#" dcat > dcat.json
 cat dcterms.ttl | python convert.py "http://dublincore.org/documents/dcmi-terms/#elements-" dcterms > dcterms.json
 cat dqv.ttl | python convert.py "https://www.w3.org/TR/vocab-dqv/#dqv:" dqv > dqv.json
+cat duv.ttl | python convert.py "https://www.w3.org/TR/vocab-duv/#" duv > duv.json
 cat foaf.ttl | python convert.py "http://xmlns.com/foaf/spec/#" foaf > foaf.json
- cat owl.ttl | python convert.py "https://www.w3.org/TR/2012/REC-owl2-syntax-20121211/#a_" owl > owl.json
+cat ma.ttl | python convert.py "https://www.w3.org/TR/2012/REC-mediaont-10-20120209/#" ma > ma.json
+cat oa.ttl | python convert.py "https://www.w3.org/TR/annotation-vocab/#" oa > oa.json
+cat odrl.ttl | python convert.py "https://www.w3.org/TR/odrl-vocab/#term-" odrl > odrl.json
+cat owl.ttl | python convert.py "https://www.w3.org/TR/2012/REC-owl2-syntax-20121211/#a_" owl > owl.json
 cat prov.ttl | python convert.py "https://www.w3.org/TR/2013/REC-prov-o-20130430/#" "" > prov.json
+cat qb.ttl | python convert.py "https://www.w3.org/TR/vocab-data-cube/#dfn-qb-" qb > qb.json
+cat rdf.ttl | python convert.py "https://www.w3.org/TR/2004/REC-rdf-mt-20040210/#" rdf > rdf.json
 cat rdfs.ttl | python convert.py "https://www.w3.org/TR/rdf-schema/#ch_" rdfs > rdfs.json
+cat rr.ttl | python convert.py "https://www.w3.org/ns/r2rml#" rr > rr.json
 cat shacl.ttl | python convert.py "https://www.w3.org/TR/shacl/#" sh > shacl.json
 cat skos.ttl | python convert.py "https://www.w3.org/2009/08/skos-reference/skos.html#" skos > skos.json
